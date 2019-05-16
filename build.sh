9a4a03f4bcd1654e6d20cc22b931e3b89adde716
David Potolski Lafetá <davidpotolskilafeta@gmail.com>
2019-05-16 19:46:04 -0300

    Auto stash before merge of "master" and "origin/master"

--- build.sh
+++ build.sh
@@ -1,12 +1,20 @@
 #If you're not using vcscode (you should) run this .sh to build and execute project
-
 mkdir -p build
+
 cd build
+
 echo "##### Creating Makefile ######"
+
 cmake ..
+
 echo "#### Compiling ####"
+
 make
+
 cd ..
+
 echo "#### Running `sb_tp1_2019-1`####"
+
 ./sb_tp1_2019-1
+
 echo "Finished!"
